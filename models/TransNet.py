077545cf76b95a200eed3cb7cbc06ba6bb32f84a
Treedy2020 <75835140+Treedy2020@users.noreply.github.com>
2022-01-22 00:06:33 +0800

    Update TransNet.py

--- models/TransNet.py
+++ models/TransNet.py
@@ -15,41 +15,13 @@ __all__ = ["transnet"]
 
 Tensor = torch.Tensor
 
-#位编码部分✔
+
 def positional_encoding(X, num_features, dropout_p=0.1, max_len=512) -> Tensor:
-    r'''
-        给输入加入位置编码
-    参数：
-        - num_features: 所取得特征向量得维度
-        - dropout_p: dropout的概率，当其为非零时执行dropout
-        - max_len: ，默认512
-
-    形状：
-        - 输入： [batch_size, seq_length, num_features]
-        - 输出： [batch_size, seq_length, num_features]
-
-    例子：
-        >>> X = torch.randn((2,4,10))
-        >>> X = positional_encoding(X, 10)
-        >>> print(X.shape)
-        >>> torch.Size([2, 4, 10])
-    '''
-    """
-    dropout = nn.Dropout(dropout_p)
-    P = torch.zeros((1, max_len, num_features))
-    X_ = torch.arange(max_len, dtype=torch.float32).reshape(-1, 1) / torch.pow(
-        10000,
-        torch.arange(0, num_features, 2, dtype=torch.float32) / num_features)
-    P[:, :, 0::2] = torch.sin(X_)
-    P[:, :, 1::2] = torch.cos(X_)
-    X = X + P[:, :X.shape[1], :].to(X.device)
-    """
     num_features_ = num_features
     max_len_ = max_len
     dropout = nn.Dropout(dropout_p)
     return X
 
-#点积注意力机制 ✔
 def scale_dot_attention(
        q:Tensor,
        k:Tensor,
@@ -57,12 +29,7 @@ def scale_dot_attention(
        dropout_p:float = 0.0,
        attn_mask : Optional[Tensor] = None,
 )-> Tuple[Tensor,Tensor]:
-    r'''
-    q:(B,N_target,N_impressed)
-    k:(B,N_source,N_impressed)
-    v:(B,N_source,N_impressed)
-    attn_mask: (B,N_target,N_source) or (N_target,N_source)
-    '''
+    
     _,_,E = q.shape
     q = q / math.sqrt(E)
     attn = torch.bmm(q,k.transpose(-2,-1))
@@ -75,16 +42,8 @@ def scale_dot_attention(
     out = torch.bmm(attn,v)
 
     return out,attn
-"""
-q = torch.randn((2,32,32))
-k = torch.randn((2,32,32))
-v = torch.randn((2,32,32))
-out_1 = scale_dot_attention(q,k,v)
-print(out_1[0].shape)
-print(out_1[1].shape)
-"""
 
-#1.用query,key,value对x实现线性映射得到q，k，v；2.遮挡机制；3.点积注意力机制
+
 def multi_head_attention_forward(
         query: Tensor,
         key: Tensor,
@@ -104,18 +63,7 @@ def multi_head_attention_forward(
         k_proj_weight: Optional[Tensor] = None,
         v_proj_weight: Optional[Tensor] = None,
 ) -> Tuple[Tensor, Optional[Tensor]]:
-    r'''
-    形状：
-        输入：
-        - query：`(L, N, E)`
-        - key: `(S, N, E)`
-        - value: `(S, N, E)`
-        - key_padding_mask: `(N, S)`
-        - attn_mask: `(L, S)` or `(N * num_heads, L, S)`
-        输出：
-        - attn_output:`(L, N, E)`
-        - attn_output_weights:`(N, L, S)`
-    '''
+    
     tgt_len, bsz, embed_dim = query.shape
     src_len, _, _ = key.shape
     head_dim = embed_dim // num_heads
@@ -148,8 +96,7 @@ def multi_head_attention_forward(
             "Byte tensor for key_padding_mask in nn.MultiheadAttention is deprecated. Use bool tensor instead.")
         key_padding_mask = key_padding_mask.to(torch.bool)
 
-    # reshape q,k,v将Batch放在第一维以适合点积注意力
-    # 同时为多头机制，将不同的头拼在一起组成一层
+
     q = q.contiguous().view(tgt_len, bsz * num_heads, head_dim).transpose(0, 1)
     k = k.contiguous().view(-1, bsz * num_heads, head_dim).transpose(0, 1)
     v = v.contiguous().view(-1, bsz * num_heads, head_dim).transpose(0, 1)
@@ -164,13 +111,13 @@ def multi_head_attention_forward(
             attn_mask = attn_mask.logical_or(key_padding_mask)
         else:
             attn_mask = attn_mask.masked_fill(key_padding_mask, float("-inf"))
-    # 若attn_mask值是布尔值，则将mask转换为float
+  
     if attn_mask is not None and attn_mask.dtype == torch.bool:
         new_attn_mask = torch.zeros_like(attn_mask, dtype=torch.float)
         new_attn_mask.masked_fill_(attn_mask, float("-inf"))
         attn_mask = new_attn_mask
 
-    # 若training为True时才应用dropout
+    
     if not training:
         dropout_p = 0.0
     attn_output, attn_output_weights = scale_dot_attention(q, k, v, attn_mask, dropout_p)
@@ -191,26 +138,6 @@ def _in_projection_packed(
     w: Tensor,
     b: Optional[Tensor] = None,
 ) -> List[Tensor]:
-    r"""
-    用一个大的权重参数矩阵进行线性变换
-
-    参数:
-        q, k, v: 对自注意来说，三者都是src；对于seq2seq模型，k和v是一致的tensor。
-                 但它们的最后一维(num_features或者叫做embed_dim)都必须保持一致。
-        w: 用以线性变换的大矩阵，按照q,k,v的顺序压在一个tensor里面。
-        b: 用以线性变换的偏置，按照q,k,v的顺序压在一个tensor里面。
-
-    形状:
-        输入:
-        - q: shape:`(..., E)`，E是词嵌入的维度（下面出现的E均为此意）。
-        - k: shape:`(..., E)`
-        - v: shape:`(..., E)`
-        - w: shape:`(E * 3, E)`
-        - b: shape:`E * 3`
-
-        输出:
-        - 输出列表 :`[q', k', v']`，q,k,v经过线性变换前后的形状都一致。
-    """
     E = q.size(-1)
     # 若为自注意，则q = k = v = src，因此它们的引用变量都是src
     # 即k is v和q is k结果均为True
@@ -237,16 +164,6 @@ def _in_projection_packed(
 
 
 class MultiheadAttention(nn.Module):
-    r'''
-    参数：
-        embed_dim: 词嵌入的维度
-        num_heads: 平行头的数量
-        batch_first: 若`True`，则为(batch, seq, feture)，若为`False`，则为(seq, batch, feature)
-
-    例子：
-        >>> multihead_attn = MultiheadAttention(embed_dim, num_heads)
-        >>> attn_output, attn_output_weights = multihead_attn(query, key, value)
-    '''
 
     def __init__(self, embed_dim, num_heads, dropout=0., bias=True,
                  kdim=None, vdim=None, batch_first=False) -> None:
@@ -324,38 +241,7 @@ class MultiheadAttention(nn.Module):
         else:
             return attn_output, attn_output_weights
 
-
-"""
-src   = torch.randn((2,32,32))
-src_q = torch.randn((2,8,16))
-src_k = torch.randn((2,8,16))
-src_v = torch.randn((2,8,16))
-src = positional_encoding(src,32,0.1)
-print(src.shape)
-multi_head_atten_1 = MultiheadAttention(16,8,0.1)
-atten_output,atten_output_weights = multi_head_atten_1(src_q,src_k,src_v)
-print(atten_output.shape,atten_output_weights.shape)
-"""
-
-#搭建TransNet
 class TransformerEncoderLayer(nn.Module):
-    r'''
-    参数：
-        d_model: 输入矩阵（行、列）向量的维度（必备）
-        nhead: 多头注意力中平行头的数目（必备）
-        dim_feedforward: 全连接层的神经元的数目，又称经过此层输入的维度（Default = 2048）
-        dropout: dropout的概率（Default = 0.1）
-        activation: 两个线性层中间的激活函数，默认relu或gelu
-        lay_norm_eps: layer normalization中的微小量，防止分母为0（Default = 1e-5）
-        batch_first: 若`True`，则为(batch, seq, feture)，若为`False`，则为(seq, batch, feature)（Default：False）
-
-    例子：
-         encoder_layer = TransformerEncoderLayer(d_model=32, nhead=8)
-         src = torch.randn((2, 4, 32))#注意第二维实际上是需要进行压缩的维度
-         out = encoder_layer(src)
-         print(out.shape)
-         >> torch.Size([2, 4, 32])
-    '''
 
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation=F.relu,
                  layer_norm_eps=1e-5, batch_first=False) -> None:
@@ -385,23 +271,8 @@ class TransformerEncoderLayer(nn.Module):
         src = self.norm2(src)
         return src
 
-#用Transformer layer 组成Encoder
-class TransformerEncoder(nn.Module):
-    r'''
-    参数：
-        d_model ：32，输入时矩阵（行、列）压缩向量的维度
-        encoder_layer（必备）
-        num_layers： encoder_layer的层数（必备）
-        norm: 归一化的选择（可选）
 
-    例子：
-     encoder_layer = TransformerEncoderLayer(d_model=32, nhead=8)
-     transformer_encoder = TransformerEncoder(encoder_layer, num_layers=6)
-     src = torch.randn((2, 32, 32))
-     out = transformer_encoder(src)
-     print(out.shape)
-     torch.Size([2, 32, 32])
-    '''
+class TransformerEncoder(nn.Module):
 
     def __init__(self, encoder_layer, num_layers, norm=None):
         super(TransformerEncoder, self).__init__()
@@ -423,25 +294,6 @@ class TransformerEncoder(nn.Module):
 
 #Decoder Layer:
 class TransformerDecoderLayer(nn.Module):
-    r'''
-    参数：
-        d_model ：32，输入时矩阵（行、列）压缩向量的维度
-        nhead: 多头注意力中平行头的数目（必备）
-        dim_feedforward: 全连接层的神经元的数目，又称经过此层输入的维度（Default = 2048）
-        dropout: dropout的概率（Default = 0.1）
-        activation: 两个线性层中间的激活函数，默认relu或gelu
-        lay_norm_eps: layer normalization中的微小量，防止分母为0（Default = 1e-5）
-        batch_first: 若`True`，则为(batch, seq, feture)，若为`False`，则为(seq, batch, feature)（Default：False）
-
-    例子：
-        decoder_layer = nn.TransformerDecoderLayer(d_model=32, nhead=8)
-        memory = torch.randn((2, 16, 32))
-        tgt = torch.randn((2, 32, 32)) #目标矩阵的维度
-        out = decoder_layer(tgt, memory)
-        print(out.shape)
-        >>torch.Size([2, 32, 32])
-    '''
-
     def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1, activation=F.relu,
                  layer_norm_eps=1e-5, batch_first=False) -> None:
         super(TransformerDecoderLayer, self).__init__()
@@ -465,15 +317,6 @@ class TransformerDecoderLayer(nn.Module):
     def forward(self, tgt: Tensor, memory: Tensor, tgt_mask: Optional[Tensor] = None,
                 memory_mask: Optional[Tensor] = None, tgt_key_padding_mask: Optional[Tensor] = None,
                 memory_key_padding_mask: Optional[Tensor] = None) -> Tensor:
-        r'''
-        参数：
-            tgt: 目标语言序列（必备）
-            memory: 从最后一个encoder_layer跑出的句子（必备）
-            tgt_mask: 目标语言序列的mask（可选）
-            memory_mask（可选）
-            tgt_key_padding_mask（可选）
-            memory_key_padding_mask（可选）
-        '''
         tgt2 = self.self_attn(tgt, tgt, tgt, attn_mask=tgt_mask,
                               key_padding_mask=tgt_key_padding_mask)[0]
         tgt = tgt + self.dropout1(tgt2)
@@ -489,15 +332,7 @@ class TransformerDecoderLayer(nn.Module):
 
 #Decoder
 class TransformerDecoder(nn.Module):
-    r'''
-    参数：
-        decoder_layer（必备）
-        num_layers: decoder_layer的层数（必备）
-        norm: 归一化选择
-
-    例子：
-    '''
-
+    
     def __init__(self, decoder_layer, num_layers, norm=None):
         super(TransformerDecoder, self).__init__()
         self.layer = decoder_layer
@@ -519,27 +354,6 @@ class TransformerDecoder(nn.Module):
         return output
 
 class Transformer(nn.Module):
-    r'''
-    参数：
-        d_model ：输入矩阵嵌入的维度（必备）（Default = 32），
-        nhead: 多头注意力中平行头的数目（必备）（Default=8）
-        num_encoder_layers:编码层层数（Default=8）
-        num_decoder_layers:解码层层数（Default=8）
-        dim_feedforward: 全连接层的神经元的数目，又称经过此层输入的维度（Default = 2048）
-        dropout: dropout的概率（Default = 0.1）
-        activation: 两个线性层中间的激活函数，默认relu或gelu
-        custom_encoder: 自定义encoder（Default=None）
-        custom_decoder: 自定义decoder（Default=None）
-        lay_norm_eps: layer normalization中的微小量，防止分母为0（Default = 1e-5）
-        batch_first: 若`True`，则为(batch, seq, feture)，若为`False`，则为(seq, batch, feature)（Default：False）
-
-    例子：
-       src = torch.rand((2, 32, 32))
-       tgt = torch.rand((2, 32, 32))
-       out = transformer_model(src, tgt)
-       print(out.shape)
-       >>torch.Size([2, 32, 32])
-    '''
 
     def __init__(self, d_model: int = 512, nhead: int = 8, num_encoder_layers: int = 6,
                  num_decoder_layers: int = 6, dim_feedforward: int = 2048, dropout: float = 0.1,
@@ -577,41 +391,7 @@ class Transformer(nn.Module):
                     memory_mask: Optional[Tensor] = None, src_key_padding_mask: Optional[Tensor] = None,
                     tgt_key_padding_mask: Optional[Tensor] = None,
                     memory_key_padding_mask: Optional[Tensor] = None) -> Tensor:
-            r'''
-            参数：
-                src: 源CSI矩阵（送入Encoder）（必备）
-                tgt: 目标CSI矩阵（送入Decoder）（必备）
-                src_mask: （可选)
-                tgt_mask: （可选）
-                memory_mask: （可选）
-                src_key_padding_mask: （可选）
-                tgt_key_padding_mask: （可选）
-                memory_key_padding_mask: （可选）
-
-            形状：
-                - src: shape:`(S, N, E)`, `(N, S, E)` if batch_first.
-                - tgt: shape:`(T, N, E)`, `(N, T, E)` if batch_first.
-                - src_mask: shape:`(S, S)`.
-                - tgt_mask: shape:`(T, T)`.
-                - memory_mask: shape:`(T, S)`.
-                - src_key_padding_mask: shape:`(N, S)`.
-                - tgt_key_padding_mask: shape:`(N, T)`.
-                - memory_key_padding_mask: shape:`(N, S)`.
-
-                [src/tgt/memory]_mask确保有些位置不被看到，如做decode的时候，只能看该位置及其以前的，而不能看后面的。
-                若为ByteTensor，非0的位置会被忽略不做注意力；若为BoolTensor，True对应的位置会被忽略；
-                若为数值，则会直接加到attn_weights
-
-                [src/tgt/memory]_key_padding_mask 使得key里面的某些元素不参与attention计算，三种情况同上
-
-                - output: shape:`(T, N, E)`, `(N, T, E)` if batch_first.
-
-            注意：
-                src和tgt的最后一维需要等于d_model，batch的那一维需要相等
-
-            例子:
-                >>> output = transformer_model(src, tgt, src_mask=src_mask, tgt_mask=tgt_mask)
-            '''
+           
             memory = self.encoder(src.view(-1,64,32), mask=src_mask, src_key_padding_mask=src_key_padding_mask)
             memory_encoder = self.fc_encoder(memory.view(memory.shape[0],-1))
             memory_decoder = self.fc_decoder(memory_encoder).view(-1,64,32)
@@ -623,14 +403,14 @@ class Transformer(nn.Module):
             return output
 
     def generate_square_subsequent_mask(self, sz: int) -> Tensor:
-        r'''产生关于序列的mask，被遮住的区域赋值`-inf`，未被遮住的区域赋值为`0`'''
+        
         mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
 
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
 
     def _reset_parameters(self):
-        r'''用正态分布初始化参数'''
+   
         for p in self.parameters():
             if p.dim() > 1:
                 xavier_uniform_(p)
@@ -640,7 +420,7 @@ def transnet(reduction=64):
     r""" Create a proposed TransNet.
 
         :param reduction: the reciprocal of compression ratio
-        :return: an instance of CRNet
+        :return: an instance of TransNet
         """
     model = Transformer(d_model=32,num_encoder_layers=2,num_decoder_layers=2,nhead=2,reduction =reduction,dropout= 0.)
     return model
